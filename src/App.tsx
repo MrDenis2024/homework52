4800bb52b44a861db465ccb7727c9b4a2714beee
Khrunev Denis <deniskhrunev@gmail.com>
2024-05-20 20:07:10 +0600

    Уровень 2 готов

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css';
 import GameCard from "./components/GameCard/GameCard.tsx";
+import CardDeck from "./lib/CardDeck.ts";
+import {useState} from "react";
+import Card from "./lib/Card.ts";
+
+
 
 const App = () => {
+    const [cards, setCards] = useState<Card[]>([]);
+    const [deck] = useState(new CardDeck());
+
+    const giveCards = () => {
+        const dealtCards = deck.getCards(5);
+        setCards(dealtCards);
+    };
 
   return (
       <>
-        <div className="playingCards faceImages">
-          <GameCard rank={'10'} suit={'hearts'}/>
-        </div>
+          <button onClick={giveCards}>asd</button>
+          <div className="playingCards faceImages">
+              <GameCard rank={'10'} suit={'hearts'}/>
+          </div>
       </>
   );
 };
---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,35 @@
+import Card from './Card';
+
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const suits = ['diams', 'hearts', 'clubs', 'spades'];
+
+class CardDeck {
+    public cardList: Card[] = [];
+
+    constructor() {
+        this.сreationCardList();
+    }
+
+    сreationCardList = () => {
+        for (const suit of suits) {
+            for (const rank of ranks) {
+                this.cardList.push(new Card(rank, suit));
+            }
+        }
+    };
+
+    getCard(): Card {
+        const randomIndex = Math.floor(Math.random() * this.cardList.length);
+        return this.cardList.splice(randomIndex, 1)[0];
+    }
+
+    getCards(howMany: number): Card[] {
+        const cards: Card[] = [];
+        for (let i = 0; i < howMany; i++) {
+            cards.push(this.getCard());
+        }
+        return cards;
+    }
+}
+
+export default CardDeck;
